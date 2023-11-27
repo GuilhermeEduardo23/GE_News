35ce8e6b24785d4acd0dce9ddc985da5cbf0592a
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-11-27 17:26:10 +0000

    Finalização do arquivo DateTime e de otimização da Header. Início da section

---
+++ scripts/DateTime.js
@@ -0,0 +1,22 @@
+const showDateTime = () => {
+    const dateTime = document.getElementById('data_hora');
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let greetings;
+
+    /* Greetings */
+    if(hour >= 0 && hour <= 11) greetings = 'Bom dia';
+    else if(hour >= 12 && hour <= 18) greetings = 'Boa tarde';
+    else greetings = 'Boa noite';
+
+    if(hour < 10) hour = '0' + hour;
+    if(minute < 10) minute = '0' + minute;
+
+    dateTime.innerText = `${greetings}! ${day}/${month}/${year} - ${hour}:${minute}`;
+}
+
+showDateTime();
