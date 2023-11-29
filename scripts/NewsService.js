8668282a3a0442b578bae9b0eb8ec7f6d1763ad4
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-11-29 11:21:02 +0000

    Integração da Api com o visual

--- scripts/NewsService.js
+++ scripts/NewsService.js
@@ -1,32 +1,32 @@
+const container = document.querySelector('.main');
+
 async function showNotices() {
-    let title = document.getElementsByClassName('titulo');
+    /*let title = document.getElementsByClassName('titulo');
     let author = document.getElementsByClassName('autor');
-    let link = document.getElementsByClassName('link');
-    let principal = document.getElementsByClassName('main');
+    let link = document.getElementsByClassName('link');*/
     
     const key = 'e4590769245f4569b9e9dc8e3cec6eb8';
     const url = `https://newsapi.org/v2/top-headlines?country=br&apiKey=${key}`;
 
     try {
         const response = await fetch(url);
-        const data = await response.json();
-        const {articles} = data;
+        let data = await response.json();
+        let {articles} = data;
         
-        for(let i = 0; i < articles.length; i++) {
-            title += articles[i].title;
-            author += articles[i].author;
-            link += articles[i].url;
-            
-            principal.innerHTML += `
-            <section class="main sections">
-                <h1 class="titulo">${title}</h1>
-                <p class="autor">${author}</p>
-                <a class="link" href="${link}" target="_blank" title="${link}">Clique aqui e saiba mais!</a>
-            </section>`;
-        }
 
+        articles.map(item => {
+            let noticias = document.createElement('section');
+
+            noticias.innerHTML = `
+            <h1 class="titulo">${item.title}</h1>
+            <p class="autor">${item.author}</p>
+            <a class="link" target="_blank" title="${item.url}" href="${item.url}">Clique aqui e saiba mais!</a>
+            `;
+
+            container.appendChild(noticias);
+        })
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
 }
 
