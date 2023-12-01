68fcb3625bb7034ac420362db028cf84c498de3d
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-12-01 22:19:14 +0000

    Finalização do projeto

--- scripts/NewsService.js
+++ scripts/NewsService.js
@@ -8,25 +8,24 @@ async function showNotices() {
         const response = await fetch(url);
         let data = await response.json();
         let {articles} = data;
-        console.log(response);
 
-        if(response.status === 426 || response.status === 200) {
-            document.write('No momento o GE News está em manutenção! Voltaremos em breve.');
+        if(response.status !== 200) {
+            document.write('No momento estamos em manutenção! Logo voltaremos.');
+        } else {
+            articles.map(item => {
+                let noticias = document.createElement('section');
+                noticias.classList.add('main');
+                noticias.classList.add('sections');
+    
+                noticias.innerHTML = `
+                <h1 class="titulo">${item.title}</h1>
+                <p class="autor"><strong>Fonte:</strong> ${item.author}</p>
+                <a class="link" target="_blank" title="${item.url}" href="${item.url}">Ir para ${item.author}</a>
+                `;
+    
+                container.appendChild(noticias);
+            })
         }
-
-        articles.map(item => {
-            let noticias = document.createElement('section');
-            noticias.classList.add('main');
-            noticias.classList.add('sections');
-
-            noticias.innerHTML = `
-            <h1 class="titulo">${item.title}</h1>
-            <p class="autor"><strong>Fonte:</strong> ${item.author}</p>
-            <a class="link" target="_blank" title="${item.url}" href="${item.url}">Ir para ${item.author}</a>
-            `;
-
-            container.appendChild(noticias);
-        })
     } catch (error) {
         console.log(error);
     }
