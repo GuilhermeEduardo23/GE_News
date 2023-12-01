75b59803c2b05e4600c03104e75c5846a4580170
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-12-01 21:43:34 +0000

    Correção de Bugs

--- scripts/NewsService.js
+++ scripts/NewsService.js
@@ -1,13 +1,14 @@
 const container = document.querySelector('.main');
 
 async function showNotices() {  
-    const key = 'e4590769245f4569b9e9dc8e3cec6eb8';
-    const url = `https://newsapi.org/v2/top-headlines?country=br&apiKey=${key}`;
+    //const key = 'e4590769245f4569b9e9dc8e3cec6eb8';
+    const url = `https://newsapi.org/v2/top-headlines?country=br&apiKey=e4590769245f4569b9e9dc8e3cec6eb8`;
 
     try {
         const response = await fetch(url);
         let data = await response.json();
         let {articles} = data;
+        console.log(articles);
 
         articles.map(item => {
             let noticias = document.createElement('section');
@@ -17,13 +18,14 @@ async function showNotices() {
             noticias.innerHTML = `
             <h1 class="titulo">${item.title}</h1>
             <p class="autor"><strong>Fonte:</strong> ${item.author}</p>
-            <a class="link" target="_blank" title="${item.url}" href="${item.url}">Acessar ao site ${item.author}</a>
+            <a class="link" target="_blank" title="${item.url}" href="${item.url}">Ir para ${item.author}</a>
             `;
 
             container.appendChild(noticias);
         })
     } catch (error) {
         console.log(error);
+        noticias.innerText = error;
     }
 }
 
